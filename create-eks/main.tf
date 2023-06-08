9cf65b3f0f3b994ad3749c9f7050d61fe0012c75
Kostis Kapelonis <kostis@codefresh.io>
2023-06-08 15:23:59 +0300

    Change node groups

--- create-eks/main.tf
+++ create-eks/main.tf
@@ -93,8 +93,8 @@ module "eks" {
       instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 4
+      desired_size = 3
     }
 
     two = {
@@ -103,8 +103,8 @@ module "eks" {
       instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      max_size     = 4
+      desired_size = 3
     }
   }
 }
