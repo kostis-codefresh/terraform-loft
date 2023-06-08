bcf74c6847ea8117a21e6f6c4ded84751fc7c8cd
Kostis Kapelonis <kostis@codefresh.io>
2023-06-08 14:58:48 +0300

    Exposed loft

---
+++ multiple-clusters-remote/main.tf
@@ -0,0 +1,49 @@
+terraform {
+
+  backend "kubernetes" {
+    secret_suffix = "multiple-clusters"
+    config_path   = "~/.kube/config"
+  }
+  required_providers {
+    loft = {
+      source = "registry.terraform.io/loft-sh/loft"
+    }
+  }
+
+
+}
+
+provider "loft" {}
+
+resource "loft_space_instance" "example-space" {
+  metadata {
+    namespace = "loft-p-default"
+    name      = "example-space2"
+  }
+  spec {
+    template_ref {
+      name = "isolated-space"
+    }
+  }
+}
+
+resource "loft_virtual_cluster_instance" "example-vcluster" {
+
+  count = 5
+
+  metadata {
+    namespace = "loft-p-default"
+    name      = "example-vcluster-${count.index}"
+  }
+  spec {
+    owner {
+      user = "admin"
+    }
+    template_ref {
+      name = "isolated-vcluster"
+    }
+  }
+}
+
+
+
