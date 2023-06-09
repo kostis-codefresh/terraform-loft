6215dee4e7a9a8fce7d4e8bf0f09706518ff880d
Kostis Kapelonis <kostis@codefresh.io>
2023-06-09 11:58:55 +0300

    Remove spaces, show only clusters

--- single-cluster/main.tf
+++ single-cluster/main.tf
@@ -15,30 +15,20 @@ terraform {
 
 provider "loft" {}
 
-resource "loft_space_instance" "example-space" {
-  metadata {
-    namespace = "loft-p-default"
-    name      = "example-space2"
-  }
-  spec {
-    template_ref {
-      name = "isolated-space"
-    }
-  }
-}
+
 
 resource "loft_virtual_cluster_instance" "example-vcluster" {
 
   metadata {
     namespace = "loft-p-default"
-    name      = "example-vcluster2"
+    name      = "my-vcluster-tf"
   }
   spec {
     owner {
       user = "admin"
     }
-    template_ref {
-      name = "isolated-vcluster"
+    template {
+
     }
   }
 }
